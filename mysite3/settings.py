13dfae32c2fa18e082da1290a44b1bb7815338a2
nadermasry <144729013+nadermasry@users.noreply.github.com>
2023-11-25 16:15:30 +0200

    Update settings.py

--- mysite3/settings.py
+++ mysite3/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xvam_dn-ige)%+*3z%1hqy)y6+6ttm!axv_7(_!dhi4d)de4a^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
